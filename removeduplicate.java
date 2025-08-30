ebd30fe64066a0c254441641ad1c51fe58fa556e
kaushlendra singh <thakurkaushlendra32@gmail.com>
2025-08-31 00:42:49 +0530

    new file

---
+++ removeduplicate.java
@@ -0,0 +1,26 @@
+class removeduplicate {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        removeduplicate obj = new removeduplicate();
+
+        int[] arr = {1, 1, 2, 2, 3, 4, 4, 5, 6};
+        int k = obj.removeDuplicates(arr);
+
+        System.out.println("Number of unique elements: " + k);
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
