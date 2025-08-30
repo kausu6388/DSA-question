7c2ec5af52b8cd34e2231c9a87c204c3594e5afe
kaushlendra singh <thakurkaushlendra32@gmail.com>
2025-08-31 00:12:47 +0530

    new file will be added

---
+++ CeilingNUM.java
@@ -0,0 +1,34 @@
+public class CeilingNUM {
+    public static void main(String[] args) {
+        int [] arr = {2,3,5,9,14,16,18};
+        int target = 15;
+        int ans = CeilingNUM(arr,target);
+        System.out.println(ans);
+        
+    }
+    // return the index of smallest number >= target
+    static int CeilingNUM(int [] arr, int target){
+        int start = 0;
+        int end  = arr.length - 1;
+        while (start<=end) {
+            // find the middle element
+            int mid  = start +(end - start)/2;
+            if (target<arr[mid]){ 
+            end = mid - 1;
+            
+                
+            } else if (target >arr[mid]){
+                start = mid + 1;
+
+            } else{
+                return mid;
+            }
+            
+
+            
+        }
+        return start;
+    }
+
+    
+}
