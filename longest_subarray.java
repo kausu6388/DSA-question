67760a3f865682bef81a82197542057bac6c66a5
kaushlendra singh <thakurkaushlendra32@gmail.com>
2025-09-20 22:55:26 +0530

    new commit

---
+++ longest_subarray.java
@@ -0,0 +1,28 @@
+public class longest_subarray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 7, 5};
+        int k = 12;
+        int n = arr.length;
+
+        int maxLength = findLongestSubarrayWithSumK(arr, n, k);
+        System.out.println("Length of the longest subarray with sum " + k + " is: " + maxLength);
+    }
+
+    public static int findLongestSubarrayWithSumK(int[] arr, int n, int k) {
+        int maxLength = 0;
+
+        for (int i = 0; i < n; i++) {
+            int currentSum = 0;
+            for (int j = i; j < n; j++) {
+                currentSum += arr[j];
+
+                if (currentSum == k) {
+                    maxLength = Math.max(maxLength, j - i + 1);
+                }
+            }
+        }
+
+        return maxLength;
+    }
+    
+}
