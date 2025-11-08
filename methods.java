169577ac7c0f395993e36caa764e5d48d2d7f517
kaushlendra singh <thakurkaushlendra32@gmail.com>
2025-11-08 21:52:04 +0530

    string concept will be coverse

---
+++ methods.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class methods {
+
+    public static void main(String[] args) {
+        String name  = "kaushlendra thakur";
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+}
---
+++ operator.java
@@ -0,0 +1,9 @@
+public class operator {
+
+    public static void main(String[] args) {
+        System.out.println('a' + 'b'); // 195
+        System.out.println("a" + "b"); // ab
+        System.out.println((char) ('a' + 2)); // c
+        System.out.println("a" + 1); // a1
+    }
+}
