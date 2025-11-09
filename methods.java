a4cf1d2459bd7b4694c8aebb656e66a5aa79c043
kaushlendra singh <thakurkaushlendra32@gmail.com>
2025-11-09 11:13:46 +0530

    new file added

--- methods.java
+++ methods.java
@@ -1,9 +1,15 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class methods {
 
     public static void main(String[] args) {
-        String name  = "kaushlendra thakur";
+        String name  = "kaushlendra thakur hello cool";
         System.out.println(Arrays.toString(name.toCharArray()));
-    }
+        System.out.println(name.toLowerCase());
+        System.out.println(name.indexOf('a'));
+        System.out.println("    kausu     ".strip());
+        System.out.println(name.replace("kaushlendra", "kalu"));
+        System.out.println(Arrays.toString(name.split(" ")));
+    }   
 }
