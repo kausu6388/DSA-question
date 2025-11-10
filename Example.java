655fa8de1a92fcce0d50927e1147abea01b8d0f3
kaushlendra singh <thakurkaushlendra32@gmail.com>
2025-11-10 22:40:45 +0530

    new file added

--- Example.java
+++ Example.java
@@ -14,19 +14,40 @@
 
 
 // print 1 to N using recursion
+// import java.util.Scanner;
+// public class Example {
+//     public static void printNumbers(int n, int N) {
+//         if(n > N) {  // base condition to stop recursion
+//             return;
+//         }
+//         System.out.println(n);
+//         printNumbers(n + 1, N);  // recursive call
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int N = sc.nextInt();
+//         printNumbers(1, N);
+//     }
+// }
+
+
+
+// pprint N to 1 using recursion
 import java.util.Scanner;
 public class Example {
-    public static void printNumbers(int n, int N) {
-        if(n > N) {  // base condition to stop recursion
+    public static void printNumbers(int n) {
+        if(n < 1) {  // base condition to stop recursion
             return;
         }
         System.out.println(n);
-        printNumbers(n + 1, N);  // recursive call
+        printNumbers(n - 1);  // recursive call
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        printNumbers(1, N);
+        printNumbers(N);
     }
 }
+
