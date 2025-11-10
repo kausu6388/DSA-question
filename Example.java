90c74038e72ce4020aef261e5cb63ee9f906d892
kaushlendra singh <thakurkaushlendra32@gmail.com>
2025-11-10 20:32:31 +0530

    new file added within a file

---
+++ Example.java
@@ -0,0 +1,32 @@
+// public class Example {
+//     public static void printNumbers(int n) {
+//         if(n > 10) {  // base condition to stop recursion
+//             return;
+//         }
+//         System.out.println(n);
+//         printNumbers(n + 1);  // recursive call
+//     }
+
+//     public static void main(String[] args) {
+//         printNumbers(1);
+//     }
+// }
+
+
+// print 1 to N using recursion
+import java.util.Scanner;
+public class Example {
+    public static void printNumbers(int n, int N) {
+        if(n > N) {  // base condition to stop recursion
+            return;
+        }
+        System.out.println(n);
+        printNumbers(n + 1, N);  // recursive call
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        printNumbers(1, N);
+    }
+}
